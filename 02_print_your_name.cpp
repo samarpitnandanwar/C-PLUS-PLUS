1411ace918759324cb5ad47da50b716089c3ad1b
SAMARPIT NANDANWAR <161283408+samarpitnandanwar@users.noreply.github.com>
2024-02-29 00:08:14 +0530

    Add files via upload

---
+++ 02_print_your_name.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string name;
+
+    cout << "Enter your name: " << endl;
+    cin >> name;
+
+    cout << "Entered name is: " << name << endl;
+
+    return 0;
+}
