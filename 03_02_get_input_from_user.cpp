1a51f97bc54a3e224e7ecf78385adceda2e0d63d
SAMARPIT NANDANWAR <161283408+samarpitnandanwar@users.noreply.github.com>
2024-03-02 11:56:55 +0530

    Add files via upload

---
+++ 03_02_get_input_from_user.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string name;
+    int age;
+
+    cin>>name>>age;
+
+
+    cout<<"your name "<< name << endl;
+    cout<<"your age "<< age << endl;
+
+
+    return 0;
+}
---
+++ 04_read_and_print_the_integer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num;
+    cout << "Enter a number: " << endl;
+    cin >> num;
+
+    cout << "Entered Number is: "<< num;
+
+    return 0;
+
+}
